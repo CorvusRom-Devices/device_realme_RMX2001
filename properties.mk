06feb28f21b48daecaa71432e88dcf2aadc27a52
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2021-06-01 09:54:19 +0530

    RMX2001: Override dalvik and bionic CPU variants
     * these are set in vendor/default.prop, lets override them with
       the correct variant (cortex-a76) which we set in boardconfig
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Change-Id: I5feb5512e700936581662fceea79311908835026
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>

--- properties.mk
+++ properties.mk
@@ -9,6 +9,16 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.google.camera \
     vendor.camera.aux.packageblacklist=org.telegram.messenger,com.microsoft.teams,com.discord
 
+# Bionic
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.bionic.cpu_variant=cortex-a76 \
+    ro.bionic.2nd_cpu_variant=cortex-a76
+
+# Dalvik
+PRODUCT_PRODUCT_PROPERTIES += \
+    dalvik.vm.isa.arm.variant=cortex-a76 \
+    dalvik.vm.isa.arm64.variant=cortex-a76
+
 # Dex2oat
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.dex2oat64.enabled=true \
