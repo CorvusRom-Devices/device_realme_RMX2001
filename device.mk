8801177f7da9184e8665c7faee5cb89e47f8a88a
Kevin F <haggertk@lineageos.org>
2021-07-08 13:51:35 +0530

    RMX2001: Don't let builds complete without vendor tree * There is zero reason to ever build without blobs. We've even seen   this situation with official builds from our servers. It's always   better for a build to fail than it is for it to produce something   that has no chance at working.
    Change-Id: I4968795670c91f691e9ecdc0e4af62e16ba3a93a
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Call proprietary blob setup
-$(call inherit-product-if-exists, vendor/realme/RMX2001/RMX2001-vendor.mk)
+$(call inherit-product, vendor/realme/RMX2001/RMX2001-vendor.mk)
 
 # Parts
 $(call inherit-product-if-exists, packages/apps/RealmeParts/parts.mk)
