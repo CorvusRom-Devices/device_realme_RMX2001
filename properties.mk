c3fd95c2f09e2e7b0e90775e228406fb4fcf7df8
Andrew Hexen <SyberHexen@gmail.com>
2021-07-09 14:52:58 +0530

    RMX2001: Disable debug.sf.latch_unsignaled * Disabling this helps reduces notification flicker, UI performance is not impacted. (per:  change https://android.googlesource.com/platform/frameworks/native/+/c5da271)
    * Fix mad lags on live wallpapers
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Signed-off-by: fakeyatogod <fakeyatokami@gmail.com>
    Change-Id: I8b7aed9bc5ff03bd8d1cd05f70c8568c1faf971c
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>

--- properties.mk
+++ properties.mk
@@ -84,7 +84,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Surfaceflinger
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     debug.sf.disable_hwc_vds=1 \
-    debug.sf.latch_unsignaled=1 \
+    debug.sf.latch_unsignaled=0 \
     debug.cpurend.vsync=false \
     ro.surface_flinger.has_HDR_display=true \
     ro.surface_flinger.has_wide_color_display=true \
